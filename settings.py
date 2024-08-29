a853c0bc42145103e1dc83d3d383712d14363cd6
Levchenko Aleksandr <madlion@mail.ru>
2024-08-30 00:35:31 +0700

    regexp Changes to be committed: 	modified:   settings.py

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ import os
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SHORT_ID_REGEXP = os.getenv('SHORT_ID_REGEXP')
+    SHORT_ID_REGEXP = r'^[a-zA-Z0-9]+$'
